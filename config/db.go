682843f1ac40db736a6306d7394ac68e37c02f14
Timothy Odebunmi <dev.brymes@gmail.com>
2022-09-16 20:41:21 +0100

    Initialize DB

---
+++ config/db.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+func InitDb() *gorm.DB {
+	var err error
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_DATABASE"),
+		os.Getenv("DB_PORT"),
+	)
+
+	client, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println("Error Connecting to Database")
+		log.Fatal(err)
+	}
+	return client
+}
