de70b9bce31d361ff46164c58cb59dbcaeb99960
Timothy Odebunmi <dev.brymes@gmail.com>
2022-10-25 14:50:13 +0100

    Fix: Help Message too long

--- bot/handlers/help.go
+++ bot/handlers/help.go
@@ -8,25 +8,27 @@ import (
 )
 
 var (
-	HelpText  = formatHelpText()
+	HelpText  = formatHelpText(helpArray[:7])
+	HelpText2 = formatHelpText(helpArray[8:])
 	helpArray = [][]string{
-		{"/help", "Returns All Commands and their corresponding Descriptions"},
+		{"/help", "Display help message"},
 		{"/subscriptions", "Returns a list of commands which the server has enabled"},
-		{"/sales", "Allow to set-up a channel that feeds all NFT collection sales matching the supplied contract address from the DIA NFT Event WebSocket to a selected discord channel"},
-		{"/sales_stop", "Stops Bot from pushing sales update from a contract address or stop all bots"},
-		{"/floor", "Return floor price of the provided NFT collection contract address"},
+		{"/sales", "Get all sales for provided NFT collection to selected discord channel"},
+		{"/sales_stop", "Stops Bot from pushing sales update for NFT or stop all sales bots"},
+		{"/floor", "Return floor price of provided NFT collection address"},
 		{"/all_sales", "Return all sales above the provided threshold"},
-		{"/all_sales_stop", "Stop bot for all sales above the predetermined threshold and contract address"},
-		{"/stop_all", "Stops all bots from operating in the selected channel or stop all bots if channel is not provided"},
-		{"/set_up_info_bot", "SetUp channel to receive commands to track NFT sales for set contract address from DIA NFT WebSocket to selected channel"},
-		{"/volume", "Return volume for previously set Collection through set_up_info_bot"},
-		{"/floor_price", "Returns floor price for previously set Collection through set_up_info_bot"},
-		{"/last_trades", "Returns recent trades for previously set collections through set_up_info_bot"},
+		{"/all_sales_stop", "Stop bot for all sales above the set threshold and collection"},
+		{"/stop_all", "Stops all bots from operating in the selected channel"},
+		{"/set_up_info_bot", "Enable channel to process utility commands to track NFT sales"},
+		{"/volume", "Get volume for NFT  Collection set via set_up_info_bot"},
+		{"/floor_price", "Get floor price for NFT Collection set via set_up_info_bot"},
+		{"/last_trades", "Get recent trades for NFT collection set via set_up_info_bot"},
+		{"/stop_subscription", "Returns modal to select commands to stop"},
 	}
 )
 
 // Note: could remove SetBorder and SetRowLine if char count needs to be reduced
-func formatHelpText() string {
+func formatHelpText(helpArray [][]string) string {
 	str := &strings.Builder{}
 	table := tablewriter.NewWriter(str)
 	table.SetHeader([]string{"Help", "Message"})
@@ -50,10 +52,12 @@ func HelpHandler(discordSession *discordgo.Session, interaction *discordgo.Inter
 	if err != nil {
 		panic(err)
 	}
+	SendChannelSetupFollowUp(HelpText2, discordSession, interaction)
 }
 
 func SendHelpText(discordSession *discordgo.Session, message *discordgo.MessageCreate) {
 	_, err := discordSession.ChannelMessageSend(message.ChannelID, HelpText)
+	_, err = discordSession.ChannelMessageSend(message.ChannelID, HelpText2)
 	if err != nil {
 		panic(err)
 	}
