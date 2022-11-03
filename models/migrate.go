279e3fcc7af6f4dde7767a9ed4c000418792c56a
Timothy Odebunmi <dev.brymes@gmail.com>
2022-11-03 12:57:43 +0100

    Fix: set_up_info turning to all_sales

--- models/migrate.go
+++ models/migrate.go
@@ -22,54 +22,52 @@ func LoadCurrentSubscriptions() bool {
 	for _, subscription := range subscriptions {
 		switch subscription.Command {
 		case "sales":
-			go func() {
-				config.ActiveSalesMux.Lock()
+			config.ActiveSalesMux.Lock()
 
-				subscribedChannels := config.ActiveSales[strings.ToUpper(subscription.Address.String)][subscription.Blockchain]
-				subscribedChannels = append(subscribedChannels, subscription.ChannelID.String)
-				data := config.ActiveSales[strings.ToUpper(subscription.Address.String)]
-				if len(data) == 0 {
-					data = make(map[string][]string)
-				}
+			subscribedChannels := config.ActiveSales[strings.ToUpper(subscription.Address.String)][subscription.Blockchain]
+			subscribedChannels = append(subscribedChannels, subscription.ChannelID.String)
+			data := config.ActiveSales[strings.ToUpper(subscription.Address.String)]
+			if len(data) == 0 {
+				data = make(map[string][]string)
+			}
 
-				data[subscription.Blockchain] = utils.RemoveArrayDuplicates(subscribedChannels)
-				config.ActiveSales[strings.ToUpper(subscription.Address.String)] = data
-				config.ActiveSalesMux.Unlock()
-			}()
+			data[subscription.Blockchain] = utils.RemoveArrayDuplicates(subscribedChannels)
+			config.ActiveSales[strings.ToUpper(subscription.Address.String)] = data
+			config.ActiveSalesMux.Unlock()
+
 		case "all_sales":
-			go func() {
-				config.ActiveAllSalesMux.Lock()
 
-				subscribedChannels := config.ActiveAllSales[subscription.Threshold][subscription.Blockchain]
-				subscribedChannels = append(subscribedChannels, subscription.ChannelID.String)
+			config.ActiveAllSalesMux.Lock()
 
-				data := config.ActiveAllSales[subscription.Threshold]
-				if len(data) == 0 {
-					data = map[string][]string{}
-				}
-				data[subscription.Blockchain] = utils.RemoveArrayDuplicates(subscribedChannels)
-				config.ActiveAllSales[subscription.Threshold] = data
+			subscribedChannels := config.ActiveAllSales[subscription.Threshold][subscription.Blockchain]
+			subscribedChannels = append(subscribedChannels, subscription.ChannelID.String)
 
-				config.ActiveAllSalesKeys = make([]float64, 0, len(subscribedChannels))
+			data := config.ActiveAllSales[subscription.Threshold]
+			if len(data) == 0 {
+				data = map[string][]string{}
+			}
+			data[subscription.Blockchain] = utils.RemoveArrayDuplicates(subscribedChannels)
+			config.ActiveAllSales[subscription.Threshold] = data
 
-				for k := range config.ActiveAllSales {
-					config.ActiveAllSalesKeys = append(config.ActiveAllSalesKeys, k)
-				}
+			config.ActiveAllSalesKeys = make([]float64, 0, len(subscribedChannels))
 
-				sort.Float64s(config.ActiveAllSalesKeys)
+			for k := range config.ActiveAllSales {
+				config.ActiveAllSalesKeys = append(config.ActiveAllSalesKeys, k)
+			}
 
-				config.ActiveAllSalesMux.Unlock()
-			}()
+			sort.Float64s(config.ActiveAllSalesKeys)
+
+			config.ActiveAllSalesMux.Unlock()
+
 		case "set_up_info":
-			go func() {
-				config.ActiveSalesInfoMux.Lock()
 
-				config.ActiveSalesInfoBot[subscription.ChannelID.String] = map[string]string{
-					"address": subscription.Address.String, "blockchain": subscription.Blockchain,
-				}
+			config.ActiveSalesInfoMux.Lock()
 
-				config.ActiveSalesInfoMux.Unlock()
-			}()
+			config.ActiveSalesInfoBot[subscription.ChannelID.String] = map[string]string{
+				"address": subscription.Address.String, "blockchain": subscription.Blockchain,
+			}
+
+			config.ActiveSalesInfoMux.Unlock()
 		}
 	}
 	if len(subscriptions) > 0 {
